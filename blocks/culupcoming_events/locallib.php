b8f3249ebcbe4b10282d6f2d85886bb2feb8da04
Amanda Doughty <amanda.doughty.1@city.ac.uk>
2015-01-06 10:53:55 +0000

    Updated deprecated function

--- blocks/culupcoming_events/locallib.php
+++ blocks/culupcoming_events/locallib.php
@@ -380,7 +380,7 @@ function block_culupcoming_events_ajax_reload($lastid=0) {
                     if (!$cm = get_coursemodule_from_instance($event->modulename, $event->instance)) {
                         continue;
                     }
-                    if (!coursemodule_visible_for_user($cm)) {
+                    if (!\core_availability\info_module::is_user_visible($cm)) {
                         continue;
                     }
                 }
