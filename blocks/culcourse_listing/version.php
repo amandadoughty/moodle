5374e586122437de6e42471e136047ea4adf347d
Amanda Doughty <amanda.doughty.1@city.ac.uk>
2017-02-01 14:53:18 +0000

    Changed lang strings from period to term

--- blocks/culcourse_listing/version.php
+++ blocks/culcourse_listing/version.php
@@ -24,6 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015021204;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2017020100;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2012112900;        // Requires this Moodle version.
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '2.0.0';
 $plugin->component = 'block_culcourse_listing'; // Full name of the plugin (used for diagnostics).
