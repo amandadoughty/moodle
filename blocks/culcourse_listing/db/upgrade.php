f4d84ad9bafb8be85ca4fe8e128b66ddd4c6333d
Amanda Doughty <amanda.doughty.1@city.ac.uk>
2017-02-15 16:05:18 +0000

    Editing upgrade script

--- blocks/culcourse_listing/db/upgrade.php
+++ blocks/culcourse_listing/db/upgrade.php
@@ -22,35 +22,75 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-function xmldb_block_culcourse_listing_upgrade($oldversion = 0) {
-
+function xmldb_block_culcourse_listing_upgrade($oldversion, $block) {
     global $CFG, $DB;
+
     $dbman = $DB->get_manager(); // Get database manager
 
     if($oldversion < 2017020100) {
 		// Create new table to hold terms and ears defined in settings by date range.
         $table = new xmldb_table('block_culcourse_listing_prds');
 
-        $table->add_field('id', XMLDB_TYPE_INTEGER, '9', XMLDB_UNSIGNED, 
-            XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field(
+            'id', 
+            XMLDB_TYPE_INTEGER, 
+            '9', 
+            XMLDB_UNSIGNED, 
+            XMLDB_NOTNULL, 
+            XMLDB_SEQUENCE, 
+            null
+            );
 
-        $table->add_field('name', XMLDB_TYPE_CHAR, '255', 
-            XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null);
+        $table->add_field(
+            'name', 
+            XMLDB_TYPE_CHAR, 
+            '255', 
+            XMLDB_UNSIGNED, 
+            XMLDB_NOTNULL, 
+            null, 
+            null, 
+            'id'
+            );
 
-        $table->add_field('type', XMLDB_TYPE_INTEGER, '9', 
-            XMLDB_UNSIGNED, XMLDB_NOTNULL, null, 0);
+        $table->add_field(
+            'type', 
+            XMLDB_TYPE_INTEGER, 
+            '9', 
+            XMLDB_UNSIGNED, 
+            XMLDB_NOTNULL, 
+            null, 
+            0, 
+            'name'
+            );
 
-        $table->add_field('startdate', XMLDB_TYPE_INTEGER, '9', 
-            XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null);
+        $table->add_field(
+            'startdate', 
+            XMLDB_TYPE_INTEGER, 
+            '9', 
+            XMLDB_UNSIGNED, 
+            XMLDB_NOTNULL, 
+            null, 
+            null, 
+            'type'
+            );
 
-        $table->add_field('enddate', XMLDB_TYPE_INTEGER, '9', 
-            XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null);
+        $table->add_field(
+            'enddate', 
+            XMLDB_TYPE_INTEGER, 
+            '9', 
+            XMLDB_UNSIGNED, 
+            XMLDB_NOTNULL, 
+            null, 
+            null, 
+            'startdate'
+            );
 
         $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
-
 
         if(!$dbman->table_exists($table)) {
             $dbman->create_table($table);
         }
+
+        upgrade_block_savepoint(true, 2017020100, 'culcourse_listing');
     }
 }
