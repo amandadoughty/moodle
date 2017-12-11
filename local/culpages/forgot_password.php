faea6b813849d6b2b429ec38af7568ca1a700d2d
amandadoughty <amanda.doughty.1@city.ac.uk>
2017-12-11 13:54:56 +0000

    Creating branch CUL-3.4-feature-local_pages

---
+++ local/culpages/forgot_password.php
@@ -0,0 +1,58 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Forgot password routine.
+ *
+ * Finds the user and calls the appropriate routine for their authentication type.
+ *
+ * @package    core
+ * @subpackage auth
+ * @copyright  1999 onwards Martin Dougiamas  http://dougiamas.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require('../../config.php');
+
+// HTTPS is required in this page when $CFG->loginhttps enabled.
+$PAGE->https_required();
+
+$PAGE->set_url('/local/culpages/forgot_password.php');
+$systemcontext = context_system::instance();
+$PAGE->set_context($systemcontext);
+
+// Setup text strings.
+$strforgotten = get_string('passwordforgotten');
+$strlogin     = get_string('login');
+
+$PAGE->navbar->add($strlogin, get_login_url());
+$PAGE->navbar->add($strforgotten);
+$PAGE->set_title($strforgotten);
+$PAGE->set_heading($COURSE->fullname);
+
+// If you are logged in then you shouldn't be here!
+if (isloggedin() and !isguestuser()) {
+    redirect($CFG->wwwroot.'/my');
+}
+
+// Make sure we really are on the https page when https login required.
+$PAGE->verify_https_required();
+
+// DISPLAY FORM.
+
+echo $OUTPUT->header();
+echo get_string('passwordforgottentxt', 'local_culpages');
+echo $OUTPUT->footer();
---
+++ local/culpages/lang/en/local_culpages.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'local_culactivity_stream', language 'en'
+ *
+ * @package    local
+ * @subpackage culpages
+ * @copyright  2013 Amanda Doughty <amanda.doughty.1@city.ac.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * 
+ */
+
+$string['passwordforgottentxt'] = '<h2 id="pageheading" class="main">Forgotten password?</h2>
+<p>You should login using your standard City University username and password.</p>
+<p>Make sure you are using the correct username and password, and that you have <a href="https://moodle-dev.city.ac.uk/help.php?module=moodle&amp;file=cookies.html">cookies enabled</a> in your browser.</p>
+<p>If you cannot login or you think you may have forgotten your password then please contact the IT Service Desk:</p>
+<ul>
+<li>Contact hours: Monday to Friday 8:00 – 20:30 (Term Time), 8:00 – 18:00 (Out of Term) </li>
+<li>In person: IT Service Centre (Northampton Square, Room E101)</li>
+<li>Online: <a href="https://www.city.ac.uk/itservicedesk" target="_blank">www.city.ac.uk/itservicedesk</a></li>
+<li>Phone: Ext 8181 or 020 7040 8181</li>
+</ul>';
+
+$string['heading'] = '<h2 id="pageheading" class="main">Your Computer</h2>';
