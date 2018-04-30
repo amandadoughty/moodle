7ebded415da6d3274c35e585bdbe4a4dd297be23
amandadoughty <amanda.doughty.1@city.ac.uk>
2018-04-30 15:49:44 +0100

    Playing around with DD code

---
+++ course/format/culcourse/yui/src/dragdrop/js/dragdrop.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+/**
+ * Drag and Drop for course sections and course modules.
+ *
+ * @module moodle-course-dragdrop
+ */
+
+var CSS = {
+    // ACTIONAREA: '.actions',
+    DASHLINK: 'dash',
+    QUICKLINK: 'quick',
+    ACTIVITYLINK: 'activity',
+    // ACTIVITYINSTANCE: 'activityinstance',
+    // CONTENT: 'content',
+    DASHPANEL: 'dash-panel',
+    EDITINGMOVE: 'dash_move',
+    ICONCLASS: 'iconsmall',
+    // JUMPMENU: 'jumpmenu',
+    // LEFT: 'left',
+    // LIGHTBOX: 'lightbox',
+    // MOVEDOWN: 'movedown',
+    // MOVEUP: 'moveup',
+    // PAGECONTENT: 'page-content',
+    // RIGHT: 'right',
+    QUICKLINKSCONTAINER: 'quicklinks',
+    ACTIVITYLINKSCONTAINER: 'activitylinks',
+    // SECTIONADDMENUS: 'section_add_menus',
+    // SECTIONHANDLE: 'section-handle',
+    // SUMMARY: 'summary',
+    // SECTIONDRAGGABLE: 'sectiondraggable'
+};
+
+M.course = M.course || {};
