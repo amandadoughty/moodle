ff033a4b6ff49c8f08eaf6c86ec8546f35ba8599
amandadoughty <amanda.doughty.1@city.ac.uk>
2018-06-01 20:14:24 +0100

    Fixed Fatal error: Namespace declaration statement has to be the very first statement or after any declare call in the script in /moodle/moodle-git/theme/cul_boost/classes/output/core_renderer.php on line 19

--- theme/cul_boost/classes/output/core_renderer.php
+++ theme/cul_boost/classes/output/core_renderer.php
@@ -14,9 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-defined('MOODLE_INTERNAL') || die;
-
 namespace theme_cul_boost\output;
+
+defined('MOODLE_INTERNAL') || die;
 
 use coding_exception;
 use html_writer;
