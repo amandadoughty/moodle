730204750dd307c3022a57491371f13c91458037
amandadoughty <amanda.doughty.1@city.ac.uk>
2019-01-11 15:51:48 +0000

    Updated privacy api

--- grade/report/culuser/classes/privacy/provider.php
+++ grade/report/culuser/classes/privacy/provider.php
@@ -32,17 +32,38 @@ defined('MOODLE_INTERNAL') || die();
  * @copyright  2018 Amanda Doughty
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class provider implements \core_privacy\local\metadata\null_provider {
-	
-	use \core_privacy\local\legacy_polyfill;
+class provider implements
+    \core_privacy\local\metadata\provider,
+    \core_privacy\local\request\user_preference_provider {
 
     /**
-     * Get the language string identifier with the component's language
-     * file to explain why this plugin stores no data.
+     * Returns meta data about this system.
      *
-     * @return  string
+     * @param collection $itemcollection The initialised item collection to add items to.
+     * @return collection A listing of user data stored through this system.
      */
-    public static function _get_reason() {
-        return 'privacy:metadata';
+    public static function get_metadata(collection $items) : collection {
+        // User preferences (shared between different courses).
+        $items->add_user_preference('gradereport_culuser_view_user', 'privacy:metadata:preference:gradereport_culuser_view_user');
+
+        return $items;
+    }
+
+    /**
+     * Store all user preferences for the plugin.
+     *
+     * @param int $userid The userid of the user whose data is to be exported.
+     */
+    public static function export_user_preferences(int $userid) {
+        $prefvalue = get_user_preferences('gradereport_culuser_view_user', null, $userid);
+        if ($prefvalue !== null) {
+            $transformedvalue = transform::yesno($prefvalue);
+            writer::export_user_preference(
+                'gradereport_culuser',
+                'gradereport_culuser_view_user',
+                $transformedvalue,
+                get_string('privacy:metadata:preference:gradereport_culuser_view_user', 'gradereport_culuser')
+            );
+        }
     }
 }
