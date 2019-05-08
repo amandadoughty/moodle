d653f7be7001c4798aa53898bc7aa8949fffb7d3
gthomas2 <brudinie@gmail.com>
2019-05-08 14:58:00 +0100

    Fix JS clash with mod_bootstrapelements

--- theme/cass/amd/src/cass.js
+++ theme/cass/amd/src/cass.js
@@ -1,29 +1,55 @@
-(function() {
-    var pluginJSURL = function(path) {
-        return M.cfg.wwwroot + "/pluginfile.php/" + M.cfg.contextid + "/theme_cass/" + path;
-    };
+/**
+ * This file is part of Moodle - http://moodle.org/
+ *
+ * Moodle is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Moodle is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Theme CASS main JS file.
+ *
+ * @package   theme_cass
+ * @author    Guy Thomas <guy.thomas@tituslearning.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-    require.config({
-        enforceDefine: false,
-        paths: {
-            // Vendor code.
-            "theme_cass/TweenMax": [
-                "https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.2/TweenMax.min",
-                // CDN Fallback - whoop whoop!
-                pluginJSURL("vendorjs/TweenMax.min")
-            ]
-        },
-        shim: {
-            "theme_cass/TweenMax": {
-                exports: "TweenMax"
-            }
-        }
-    });
-})();
-
-define(['jquery', 'core/ajax', 'theme_cass/breadcrumb', 'theme_cass/TweenMax'], function($, ajax, breadcrumb, TweenMax) {
+define(['jquery', 'core/ajax', 'theme_cass/breadcrumb'], function($, ajax, breadcrumb) {
 
     return new function() {
+
+        // Add some aditional vendor requirejs AMD modules.
+        // For some reason putting this above the define causes a requirejs error when you have mod_bootstrapelements
+        // installed, so it's been moved here.
+        (function() {
+            var pluginJSURL = function(path) {
+                return M.cfg.wwwroot + "/pluginfile.php/" + M.cfg.contextid + "/theme_cass/" + path;
+            };
+
+            require.config({
+                paths: {
+                    // Vendor code.
+                    "theme_cass/TweenMax": [
+                        "https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.2/TweenMax.min",
+                        // CDN Fallback - whoop whoop!
+                        pluginJSURL("vendorjs/TweenMax.min")
+                    ]
+                },
+                shim: {
+                    "theme_cass/TweenMax": {
+                        exports: "TweenMax"
+                    }
+                }
+            });
+        })();
+
         var self = this;
 
         var scrollIn = function(selector) {
@@ -39,23 +65,24 @@ define(['jquery', 'core/ajax', 'theme_cass/breadcrumb', 'theme_cass/TweenMax'], 
         };
 
         var popCompletion = function() {
-
-            $(".next_activity_overlay").fadeTo("slow", 0, function() {
-                $(this).hide();
+            require(['theme_cass/TweenMax'], function(TweenMax) {
+                $(".next_activity_overlay").fadeTo("slow", 0, function() {
+                    $(this).hide();
+                });
+                var $aBox = $("#alertBox");
+                var $darkBg = $("#darkBackground");
+                TweenMax.to($darkBg,  0,   {display: "block"});
+                TweenMax.to($darkBg,  0.3, {background: "rgba(0,0,0,0.4)", force3D: true});
+                TweenMax.to($aBox,    0,   {left: "calc(50% - 150px)", top: "calc(50% - 150px)", delay: "0.2"});
+                TweenMax.to($aBox,    0,   {display: "block", opacity: 1, delay: "0.2"});
+                TweenMax.to($aBox,    0,   {display: "block", scale: 0.2, opacity: 0, delay: "0.2"});
+                TweenMax.to($aBox,    0.3, {opacity: 1, force3D: true, delay: "0.2"});
+                TweenMax.to($aBox,    0.3, {scale:1, force3D: true, delay: "0.2"});
+                TweenMax.to($darkBg,  0.2, {backgroundColor: "rgba(0,0,0,0)", force3D: true, delay: "2"});
+                TweenMax.to($darkBg,  0.2, {display: "none", force3D: true, delay: "2"});
+                TweenMax.to($aBox,    0.2, {opacity: 0, display: "none", force3D: true, delay: "2",
+                    onComplete: slideNextActivity});
             });
-            var $aBox = $("#alertBox");
-            var $darkBg = $("#darkBackground");
-            TweenMax.to($darkBg,  0,   {display: "block"});
-            TweenMax.to($darkBg,  0.3, {background: "rgba(0,0,0,0.4)", force3D: true});
-            TweenMax.to($aBox,    0,   {left: "calc(50% - 150px)", top: "calc(50% - 150px)", delay: "0.2"});
-            TweenMax.to($aBox,    0,   {display: "block", opacity: 1, delay: "0.2"});
-            TweenMax.to($aBox,    0,   {display: "block", scale: 0.2, opacity: 0, delay: "0.2"});
-            TweenMax.to($aBox,    0.3, {opacity: 1, force3D: true, delay: "0.2"});
-            TweenMax.to($aBox,    0.3, {scale:1, force3D: true, delay: "0.2"});
-            TweenMax.to($darkBg,  0.2, {backgroundColor: "rgba(0,0,0,0)", force3D: true, delay: "2"});
-            TweenMax.to($darkBg,  0.2, {display: "none", force3D: true, delay: "2"});
-            TweenMax.to($aBox,    0.2, {opacity: 0, display: "none", force3D: true, delay: "2",
-                onComplete: slideNextActivity});
         };
 
         this.addPopCompletion = function(mod) {
@@ -101,7 +128,7 @@ define(['jquery', 'core/ajax', 'theme_cass/breadcrumb', 'theme_cass/TweenMax'], 
                         type: "GET",
                         async:  true,
                         url: M.cfg.wwwroot + '/theme/cass/rest.php?action=get_' + type + '&contextid=' +
-                             mod.contextid, // M.cfg.context,
+                        mod.contextid, // M.cfg.context,
                         success: function(data) {
                             $('.completion-region').attr('data-content-loaded', '1');
                             $('.completion-region').html(data.html);
