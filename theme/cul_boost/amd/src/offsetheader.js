fd626e1a3d97c66479c989bfc6b9648d9895a8de
amandadoughty <amanda.doughty.1@city.ac.uk>
2019-06-04 17:28:48 +0100

    CMDLTWO-1588 Fixing scroll to section

--- theme/cul_boost/amd/src/offsetheader.js
+++ theme/cul_boost/amd/src/offsetheader.js
@@ -3,25 +3,25 @@ define(['jquery', 'core/log'], function($, log) {
     return {
         init: function() {
 
-            $('a[href*=#]:not([href=#])').click(function() 
-				{
-				if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-				|| location.hostname == this.hostname) {
+            $('a[href*="#"]:not([href="#"])').click(function() 
+                {
+                if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+                || location.hostname == this.hostname) {
 
-					var target = $(this.hash),
-					headerHeight = $(".navbar .nav-wrap.stick").height() + 5; // Get fixed header height
-					console.log(headerHeight) ;
-					target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                    var target = $(this.hash),
+                    headerHeight = $('.nav-outer-wrap').height() + 5; // Get fixed header height
+                    
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 
-					if (target.length) {
-						$('html,body').animate({
-						scrollTop: target.offset().top - headerHeight
-						}, 500);
-						return false;
-					}
-				}
-			});
-		}
+                    if (target.length) {
+                        $('html, body').animate({
+                        scrollTop: target.offset().top - headerHeight
+                        }, 500);
+                        return false;
+                    }
+                }
+            });
+        }
     }
 });
 /* jshint ignore:end */
