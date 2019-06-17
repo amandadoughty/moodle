0d8ab51951e13189dae78744abb60bbf1036fc15
amandadoughty <amanda.doughty.1@city.ac.uk>
2019-06-17 09:22:58 +0100

    Updated filter and editor plugins to v2019061200 r7.14.0

--- lib/editor/tinymce/plugins/tiny_mce_wiris/version.php
+++ lib/editor/tinymce/plugins/tiny_mce_wiris/version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018110900;
-$plugin->release = '7.8.0.1397';
+$plugin->version = 2019061200;
+$plugin->release = '7.14.0';
 $plugin->requires = 2012120300;
 $plugin->component = 'tinymce_tiny_mce_wiris';
-$plugin->dependencies = array ('filter_wiris' => 2018110900);
+$plugin->dependencies = array ('filter_wiris' => 2019061200);
 $plugin->maturity = MATURITY_STABLE;
