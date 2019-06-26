8ece1830d3fea76c9ad09963e115545698bea3cc
amandadoughty <amanda.doughty.1@city.ac.uk>
2019-06-26 11:46:21 +0100

    CMDLTWO-1637 Enhanced labels drop down not working

--- theme/cul_boost/amd/src/offsetheader.js
+++ theme/cul_boost/amd/src/offsetheader.js
@@ -3,7 +3,7 @@ define(['jquery', 'core/log'], function($, log) {
     return {
         init: function() {
 
-            $('a[href*="#"]:not([href="#"])').click(function() 
+            $('#theme-cul_boost-modulemenu a[href*="#"]:not([href="#"])').click(function() 
                 {
                 if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
                 || location.hostname == this.hostname) {
