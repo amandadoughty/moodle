362eb1c5e563f9a5665bcf07fa116d2592270ae1
amandadoughty <amanda.doughty.1@city.ac.uk>
2019-10-25 13:56:35 +0100

    Removed non-core functions from lib.php

--- theme/cul_boost/lib.php
+++ theme/cul_boost/lib.php
@@ -47,22 +47,9 @@ function theme_cul_boost_process_css($css, $theme) {
 function theme_cul_boost_page_init(moodle_page $page) {
     global $CFG, $DB, $OUTPUT, $USER, $COURSE;
 
-    $quizpageids = array(
-        'page-question-preview',
-        'page-mod-quiz-attempt'
-        );
-
     $page->requires->jquery_plugin('ui');
     $page->requires->jquery_plugin('ui-css');
-
     $page->requires->js_call_amd('theme_cul_boost/loader', 'init');
-    
-    $duration = (empty($page->theme->settings->slideduration)) ? 6 : $page->theme->settings->slideduration;
-    $opts = [
-        'duration' => $duration
-    ];
-    $page->requires->js_call_amd('theme_cul_boost/slider', 'init', [$opts]);
-
     $page->requires->js_call_amd('theme_cul_boost/dropdowns', 'init');
     $page->requires->js_call_amd('theme_cul_boost/drawermenu', 'init');
     $page->requires->js_call_amd('theme_cul_boost/navsearch', 'init');
@@ -74,7 +61,6 @@ function theme_cul_boost_page_init(moodle_page $page) {
     $page->requires->js_call_amd('theme_cul_boost/fixedbuttons', 'init');
     $page->requires->js_call_amd('theme_cul_boost/modalposition', 'init');
     $page->requires->js_call_amd('theme_cul_boost/showcourse', 'init', ['courseid' => $COURSE->id]);
-
     // Save scroll position when editing is turned on/off
     // $page->requires->js_call_amd('theme_cul_boost/savescrollpos', 'init');
     $page->requires->js_call_amd('theme_cul_boost/offsetheader', 'init');
@@ -177,262 +163,50 @@ function theme_cul_boost_store_in_localcache($filearea, $args, $options) {
 
 // theme_cul functions
 
-/**
- * Returns an object containing HTML for the areas affected by settings.
- *
- * @param renderer_base $output Pass in $OUTPUT.
- * @param moodle_page $page Pass in $PAGE.
- * @return stdClass An object with the following properties:
- *      - navbarclass A CSS class to use on the navbar. By default ''.
- *      - heading HTML to use for the heading. A logo if one is selected or the default heading.
- *      - footnote HTML to use as a footnote. By default ''.
- */
-function theme_cul_boost_get_html_for_settings(renderer_base $output, moodle_page $page) {
-    global $CFG, $COURSE;
+// /**
+//  * Returns an object containing HTML for the areas affected by settings.
+//  *
+//  * @param renderer_base $output Pass in $OUTPUT.
+//  * @param moodle_page $page Pass in $PAGE.
+//  * @return stdClass An object with the following properties:
+//  *      - navbarclass A CSS class to use on the navbar. By default ''.
+//  *      - heading HTML to use for the heading. A logo if one is selected or the default heading.
+//  *      - footnote HTML to use as a footnote. By default ''.
+//  */
+// function theme_cul_boost_get_html_for_settings(renderer_base $output, moodle_page $page) {
+//     global $CFG, $COURSE;
 
-    $return = new stdClass;
-    $return->hascourseimage = 'noimage';
-    $return->courseimage = '';
+//     $return = new stdClass;
+//     $return->hascourseimage = 'noimage';
+//     $return->courseimage = '';
 
-    if ($COURSE->id > 1) {
-        $course = new core_course_list_element($COURSE);
-        // Get course overview files.
-        $contentimages = $contentfiles = '';
-        foreach ($course->get_course_overviewfiles() as $file) {
-            $isimage = $file->is_valid_image();
-            $url = file_encode_url("$CFG->wwwroot/pluginfile.php",
-                    '/'. $file->get_contextid(). '/'. $file->get_component(). '/'.
-                    $file->get_filearea(). $file->get_filepath(). $file->get_filename(), !$isimage);
-            if ($isimage) {
-                $style = 'background-image: url('.$url.');';
-                if ($dimensions = $file->get_imageinfo()) {
-                    $ratio = $dimensions['width'] / $dimensions['height'];
-                    if ($ratio < 2) {
-                        $style .= 'background-size: auto 100%; background-position: right center; ratio:' .$ratio .';';
-                    } else {
-                        $style .= 'background-size: cover;';
-                    }
-                }
-                $return->courseimage = html_writer::tag('div','', array('style' => $style, 'class' => 'imagediv'));
-                $return->hascourseimage = 'hasimage';
-            }
-        }
-    }
+//     if ($COURSE->id > 1) {
+//         $course = new core_course_list_element($COURSE);
+//         // Get course overview files.
+//         $contentimages = $contentfiles = '';
+//         foreach ($course->get_course_overviewfiles() as $file) {
+//             $isimage = $file->is_valid_image();
+//             $url = file_encode_url("$CFG->wwwroot/pluginfile.php",
+//                     '/'. $file->get_contextid(). '/'. $file->get_component(). '/'.
+//                     $file->get_filearea(). $file->get_filepath(). $file->get_filename(), !$isimage);
+//             if ($isimage) {
+//                 $style = 'background-image: url('.$url.');';
+//                 if ($dimensions = $file->get_imageinfo()) {
+//                     $ratio = $dimensions['width'] / $dimensions['height'];
+//                     if ($ratio < 2) {
+//                         $style .= 'background-size: auto 100%; background-position: right center; ratio:' .$ratio .';';
+//                     } else {
+//                         $style .= 'background-size: cover;';
+//                     }
+//                 }
+//                 $return->courseimage = html_writer::tag('div','', array('style' => $style, 'class' => 'imagediv'));
+//                 $return->hascourseimage = 'hasimage';
+//             }
+//         }
+//     }
 
-    $return->gradebookdisclaimer = theme_cul_boost_gradebook_disclaimer($page);
+//     $return->gradebookdisclaimer = theme_cul_boost_gradebook_disclaimer($page);
 
-    return $return;
-}
+//     return $return;
+// }
 
-// @TODO move this and other functions to core renderer
-function theme_cul_boost_gradebook_disclaimer($page) {
-
-    $gradebookids = array (
-        'page-grade-report-user-index',
-        'page-grade-report-culuser-index',
-        'page-grade-report-overview-index',
-        'page-course-user'
-    );
-
-    $content = '';
-    if (in_array($page->bodyid, $gradebookids)) {
-        $disclaimer = html_writer::tag('p', get_string('gradebookdisclaimer', 'theme_cul_boost'));
-        $content = html_writer::tag('div', $disclaimer,
-            array('class' => 'alert alert-warning', 'role' => 'note'));
-    }
-    return $content;
-}
-
-/**
- * Serve the grading panel as a fragment.
- *
- * @param array $args List of named arguments for the fragment loader.
- * @return string
- */
-function theme_cul_boost_output_fragment_gradealert($args) {
-    global $CFG, $OUTPUT;
-
-    require_once($CFG->libdir.'/gradelib.php');
-    require_once($CFG->dirroot . '/mod/assign/locallib.php');
-
-    $o = '';    
-    $courseid = clean_param($args['courseid'], PARAM_INT); 
-    $assignid = clean_param($args['assignid'], PARAM_INT); 
-    $userid = clean_param($args['userid'], PARAM_INT); 
-    $context = $args['context'];
-    $cangrade = has_capability('mod/assign:grade', $context);
-
-    if ($context->contextlevel != CONTEXT_MODULE) {
-        return null;
-    }  
-   
-
-    // if($cangrade) {
-
-        $gradinginfo = grade_get_grades(
-            $courseid,
-            'mod',
-            'assign',
-            $assignid,
-            $userid
-        );
-
-        $gradingitem = null;
-        $gradebookgrade = null;
-
-        if (isset($gradinginfo->items[0])) {
-            $gradingitem = $gradinginfo->items[0];
-            $gradebookgrade = $gradingitem->grades[$userid];
-        }
-
-        if ($gradebookgrade->hidden){
-            $o .= $OUTPUT->notification(get_string('gradehidden', 'theme_cul_boost'), 'error hazard');
-        } else {
-            $o .= $OUTPUT->notification(get_string('gradenothidden', 'theme_cul_boost'), 'error hazard');
-        }
-    // }
-
-    return $o;
-}
-
-function theme_cul_boost_get_visible() {
-    global $COURSE;
-    return $COURSE->visible;
-}
-
-function theme_cul_boost_get_edituser($page) {
-    $iseditprofilepage = $page->bodyid == 'page-user-editadvanced' || $page->bodyid == 'page-user-edit';
-   
-    if($iseditprofilepage && has_capability('moodle/user:editprofile', context_system::instance())) {
-        return 'edituser';        
-    }
-
-    return '';
-}
-
-/**
- * Loads the JavaScript for the dynamic updating of the favourites menu.
- * Loads the JavaScript for the dynamic adding/removing current course from favourites. 
- *
- * @param moodle_page $page Pass in $PAGE.
- */
-function theme_cul_boost_initialise_favourites(moodle_page $page) {
-    $page->requires->js_call_amd('theme_cul_boost/favourites', 'init');
-    $page->requires->js_call_amd('theme_cul_boost/favourite', 'init');
-}
-
-/**
- * Edits the user preference 'culcourse_listing_course_favourites'
- * Adds or deletes course id's
- *
- * @param string $action add or delete
- * @param int $cid course id
- * @return array $favourites a sorted array of course id's
- */
-function theme_cul_boost_edit_favourites($action, $cid) {
-    $favourites = [];
-
-    if (is_null($myfavourites = get_user_preferences('culcourse_listing_course_favourites'))) {
-        return false;
-    } else {
-        $favourites = unserialize($myfavourites);
-    }
-
-    switch ($action) {
-        case 'add':
-            // Original block user preference setting.
-            if (!in_array($cid, $favourites)) {
-                $favourites[] = $cid;
-            }
-
-            break;
-        case 'remove':
-            // Original block user preference setting.
-            $key = array_search($cid, $favourites);
-
-            if ($key !== false) {
-                unset($favourites[$key]);
-            }
-
-            break;
-        default:
-            break;
-    }
-
-    return $favourites;
-}
-
-/**
- * Edits the favourites api.
- *
- * @param string $action add or delete
- * @param int $cid course id
- * @param int $userid user id
- * @return array $favourites a sorted array of course id's
- */
-function theme_cul_boost_edit_favourites_api($action, $cid, $userid = 0) {
-    global $USER;
-
-    $coursecontext = \context_course::instance($cid);
-    $usercontext = \context_user::instance($USER->id);
-    $ufservice = \core_favourites\service_factory::get_service_for_user_context($usercontext);
-
-    $exists = $ufservice->favourite_exists('core_course', 'courses', $cid, $coursecontext);
-
-    switch ($action) {
-        case 'add':
-            // New favourite api.
-            if (!$exists) {
-                $ufservice->create_favourite('core_course', 'courses', $cid, $coursecontext);
-            }
-
-            break;
-        case 'remove':
-            // New favourite api.
-            if ($exists) {
-                $ufservice->delete_favourite('core_course', 'courses', $cid, $coursecontext);
-            }
-
-            break;
-        default:
-            break;
-    }
-}
-
-/**
- * Sets user course favourites preference in culcourse_listing block
- *
- * @param array $favourites of course ids in sort order
- */
-function theme_cul_boost_update_favourites($favourites) {
-    // If user favourites have been transferred to the 
-    // Favourites API then do not recreate the user
-    // preference.
-    if (is_null($myfavourites = get_user_preferences('culcourse_listing_course_favourites'))) {
-        return true;
-    } 
-
-    try {
-        set_user_preference('culcourse_listing_course_favourites', serialize($favourites));
-        return true;
-    } catch (exception $e) {
-        return false;
-    }
-}
-
-/**
- * Makes hidden course visible.
- *
- * @param int $cid course id 
- */
-function theme_cul_boost_show_course($cid) {
-    global $DB;
-
-    $coursecontext = context_course::instance($cid);
-
-    if (has_capability('moodle/course:update', $coursecontext)) { 
-        return $DB->set_field('course', 'visible', 1, ['id' => $cid]);
-    }
-
-    return false;
-}
