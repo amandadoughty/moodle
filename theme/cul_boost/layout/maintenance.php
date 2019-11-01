bb4bc9b41a5880604ba2e32300672570e2db4ad0
amandadoughty <amanda.doughty.1@city.ac.uk>
2019-11-01 13:01:48 +0000

    Fixed maintenance page

--- theme/cul_boost/layout/maintenance.php
+++ theme/cul_boost/layout/maintenance.php
@@ -22,26 +22,38 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+// defined('MOODLE_INTERNAL') || die();
+
+// // user_preference_allow_ajax_update('drawer-open-nav', PARAM_ALPHA);
+// require_once($CFG->libdir . '/behat/lib.php');
+
+// $PAGE->set_popup_notification_allowed(false);
+// $isloggedin = isloggedin();
+// // Accessibility stuff.
+// $OUTPUT->standard_head_html();
+// $PAGE->requires->skip_link_to('accessibility', get_string('toaccessibility', 'theme_cul_boost'));
+// $bodyattributes = $OUTPUT->body_attributes();
+// // Block region setup
+// $hasblocks = $PAGE->blocks->region_has_content('side-post', $OUTPUT);
+// $regions = theme_cul_boost_bootstrap_grid($hasblocks);
+
+// $templatecontext = [    
+//     'output' => $OUTPUT,
+//     'isloggedin' => $isloggedin,
+//     'classes' => $regions['content'],
+//     'bodyattributes' => $bodyattributes,
+// ];
+
+// echo $OUTPUT->render_from_template('theme_cul_boost/maintenance', $templatecontext);
+
+
 defined('MOODLE_INTERNAL') || die();
 
-// user_preference_allow_ajax_update('drawer-open-nav', PARAM_ALPHA);
-require_once($CFG->libdir . '/behat/lib.php');
-
-$PAGE->set_popup_notification_allowed(false);
-$isloggedin = isloggedin();
-// Accessibility stuff.
-$OUTPUT->standard_head_html();
-$PAGE->requires->skip_link_to('accessibility', get_string('toaccessibility', 'theme_cul_boost'));
-$bodyattributes = $OUTPUT->body_attributes();
-// Block region setup
-$hasblocks = $PAGE->blocks->region_has_content('side-post', $OUTPUT);
-$regions = theme_cul_boost_bootstrap_grid($hasblocks);
-
-$templatecontext = [    
-    'output' => $OUTPUT,
-    'isloggedin' => $isloggedin,
-    'classes' => $regions['content'],
-    'bodyattributes' => $bodyattributes,
+$templatecontext = [
+    // We cannot pass the context to format_string, this layout can be used during
+    // installation. At that stage database tables do not exist yet.
+    'sitename' => format_string($SITE->shortname, true, ["escape" => false]),
+    'output' => $OUTPUT
 ];
 
 echo $OUTPUT->render_from_template('theme_cul_boost/maintenance', $templatecontext);
