f05c5548ebad0a4fd316175c94f1bcc1a1e7cd1b
amandadoughty <amanda.doughty.1@city.ac.uk>
2020-06-02 14:53:13 +0100

    Added notification popover

--- theme/cul_boost/layout/includes/header.php
+++ theme/cul_boost/layout/includes/header.php
@@ -24,14 +24,19 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+require_once(dirname(__FILE__) . '/../../../../message/output/popup/lib.php');
+
 $help = $OUTPUT->help_menu();
 $custommenu = $OUTPUT->custom_menu();
 $usermenu = $OUTPUT->user_menu();
 $userinfo = $OUTPUT->user_info();
+// $renderer = $PAGE->get_renderer('core_renderer');
+$notificationpopover = message_popup_render_navbar_output($this);
 
 $headertemplatecontext = [
     'help' => $help,
     'custommenu' => $custommenu,
     'usermenu' => $usermenu,
-    'userinfo' => $userinfo
+    'userinfo' => $userinfo,
+    'notificationpopover' => $notificationpopover
 ];
