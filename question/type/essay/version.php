5e6afdfd97142f334cf1a066d6c0300041355254
amandadoughty <m.doughty@ucl.ac.uk>
2026-02-09 15:41:55 +0000

    MDL-87915 qtype_essay: Word-Counter - Plain Text response

--- question/type/essay/version.php
+++ question/type/essay/version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_essay';
-$plugin->version   = 2025041400;
+$plugin->version   = 2026020900;
 
 $plugin->requires  = 2025040800;
 
