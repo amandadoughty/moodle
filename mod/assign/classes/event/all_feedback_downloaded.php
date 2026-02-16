f6c32f9131377f597fd69c2826ba2511cd60390e
amandadoughty <m.doughty@ucl.ac.uk>
2026-02-16 14:21:20 +0000

    MDL-45264 mod_assign: applying Sumaiya Javed patch with enhancements

---
+++ mod/assign/classes/event/all_feedback_downloaded.php
@@ -0,0 +1,117 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The mod_assign all feedback downloaded event.
+ *
+ * @package    mod_assign
+ * @copyright  2026 Amanda Doughty <m.doughty@ucl.ac.uk
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_assign\event;
+
+/**
+ * The mod_assign all feedback downloaded event.
+ *
+ * @package    mod_assign
+ * @copyright  2026 Amanda Doughty <m.doughty@ucl.ac.uk
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class all_feedback_downloaded extends base {
+    /**
+     * Flag for prevention of direct create() call.
+     * @var bool
+     */
+    protected static $preventcreatecall = true;
+
+    /**
+     * Create instance of event.
+     *
+     * @since Moodle 2.7
+     *
+     * @param \assign $assign
+     * @return all_submissions_downloaded
+     */
+    public static function create_from_assign(\assign $assign) {
+        $data = [
+            'context' => $assign->get_context(),
+            'objectid' => $assign->get_instance()->id,
+        ];
+        self::$preventcreatecall = false;
+        /** @var submission_graded $event */
+        $event = self::create($data);
+        self::$preventcreatecall = true;
+        $event->set_assign($assign);
+        return $event;
+    }
+
+    /**
+     * Returns description of what happened.
+     *
+     * @return string
+     */
+    public function get_description() {
+        return "The user with id '$this->userid' has downloaded all the feedback for the assignment " .
+            "with course module id '$this->contextinstanceid'.";
+    }
+
+    /**
+     * Return localised event name.
+     *
+     * @return string
+     */
+    public static function get_name() {
+        return get_string('eventallfeedbackdownloaded', 'mod_assign');
+    }
+
+    /**
+     * Init method.
+     *
+     * @return void
+     */
+    protected function init() {
+        $this->data['crud'] = 'r';
+        $this->data['edulevel'] = self::LEVEL_TEACHING;
+        $this->data['objecttable'] = 'assign';
+    }
+
+    /**
+     * Custom validation.
+     *
+     * @throws \coding_exception
+     * @return void
+     */
+    protected function validate_data() {
+        if (self::$preventcreatecall) {
+            throw new \coding_exception(
+                'cannot call all_feedbackdownloaded::create() directly,' .
+                ' use all_feedback_downloaded::create_from_assign() instead.'
+            );
+        }
+
+        parent::validate_data();
+    }
+
+    /**
+     * Used for mapping events on restore
+     *
+     * @return string[]
+     */
+    public static function get_objectid_mapping() {
+        return ['db' => 'assign', 'restore' => 'assign'];
+    }
+}
--- mod/assign/locallib.php
+++ mod/assign/locallib.php
@@ -695,6 +695,8 @@ class assign {
             $o .= $this->view_grading_page();
         } else if ($action == 'downloadall') {
             $o .= $this->download_submissions();
+        } else if ($action == 'downloadallfeedback') {
+            $o .= $this->download_feedbacks();
         } else if ($action == 'submit') {
             $PAGE->add_body_class('limitedwidth');
             $o .= $this->check_submit_for_grading($mform);
@@ -3719,8 +3721,8 @@ class assign {
      */
     protected function download_submissions($userids = null) {
         $downloader = new downloader($this, $userids ?: null);
-        if ($downloader->load_filelist()) {
-            $downloader->download_zip();
+        if ($downloader->load_filelist('submission')) {
+            $downloader->download_zip('submission');
         }
         // Show some notification if we have nothing to download.
         $cm = $this->get_course_module();
@@ -3738,6 +3740,36 @@ class assign {
         $result .= $renderer->continue_button($url);
         $result .= $this->view_footer();
         return $result;
+    }
+
+    /**
+     * Download a zip file of all assignment feedback.
+     *
+     * @param array|null $userids Array of user ids to download assignment submissions in a zip file
+     * @return string - If an error occurs, this will contain the error page.
+     */
+    protected function download_feedbacks($userids = null) {
+        $downloader = new downloader($this, $userids ?: null);
+        if (!empty($downloader->load_filelist('feedback'))) {
+            $downloader->download_zip('feedback');
+        }
+        // Show some notification if we have nothing to download.
+        $cm = $this->get_course_module();
+        $renderer = $this->get_renderer();
+        $header = new assign_header(
+            $this->get_instance(),
+            $this->get_context(),
+            '',
+            $cm->id,
+            get_string('downloadallfeedback', 'mod_assign')
+        );
+        $result = $renderer->render($header);
+        $result .= $renderer->notification(get_string('nofeedback', 'mod_assign'));
+        $url = new moodle_url('/mod/assign/view.php', ['id' => $cm->id, 'action' => 'grading']);
+        $result .= $renderer->continue_button($url);
+        $result .= $this->view_footer();
+        echo $result;
+        exit();
     }
 
     /**
@@ -5097,6 +5129,8 @@ class assign {
 
         if ($operation == 'downloadselected') {
             $this->download_submissions($userlist);
+        } else if ($operation == 'downloadselectedfeedback') {
+            $this->download_feedbacks($userlist);
         } else {
             foreach ($userlist as $userid) {
                 if ($operation == 'lock') {
@@ -8210,6 +8244,24 @@ class assign {
 
         return $this->cache['any_feedback_plugin_enabled'];
 
+    }
+
+    /**
+     * Check if feedback plugins installed are returning files.
+     *
+     * @return bool
+     */
+    public function is_enabled_feedback_plugin_returning_files() {
+        if (!isset($this->cache['any_feedback_plugin_returning_files'])) {
+            $this->cache['any_feedback_plugin_returning_files'] = false;
+            foreach ($this->feedbackplugins as $plugin) {
+                if ($plugin->is_enabled() && $plugin->is_visible() && $plugin->return_files()) {
+                    $this->cache['any_feedback_plugin_returning_files'] = true;
+                    break;
+                }
+            }
+        }
+        return $this->cache['any_feedback_plugin_returning_files'];
     }
 
     /**
